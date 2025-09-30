cc9d14258bc1337db69ffa32b370adfa8c33f0ef
b4esa <tysaluthfia1@gmail.com>
2025-09-30 19:46:41 +0700

    Upload File

---
+++ PRAK102-2510817220019-TysaLuthfia.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    float a = 4;
+    double b = 8;
+    double c = 3;
+    printf("Variabel X bernilai %.0f\n", a);
+    printf("Variabel Y bernilai %.0f\n", b);
+    printf("Variabel Z bernilai %.0f\n", c);
+    printf("Jumlah variabel tersebut adalah %f", a*b/c );
+    return 0;
+}
