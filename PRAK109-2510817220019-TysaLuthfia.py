3476909495e9bfdbf93ea1754240f515499fe3ce
b4esa <tysaluthfia1@gmail.com>
2025-10-02 15:49:52 +0700

    tysa cantik

--- PRAK109-2510817220019-TysaLuthfia.py
+++ PRAK109-2510817220019-TysaLuthfia.py
@@ -1,6 +1,6 @@
 PYZ = 958730
 Pahlawan = 5
 print("Jumlah pasukan yang dibawa Yu Zhong =",PYZ) 
-print("Jumlah pahlawan =", PH)
+print("Jumlah pahlawan =", Pahlawan)
 print("Jumlah pasukan yang harus dikalahkan setiap pahlawan adalah {} pasukan" .format(round (PYZ/Pahlawan))
 )
